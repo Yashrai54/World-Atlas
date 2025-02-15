60440c6f5711f3048ebd826bfab8e19e8aa9565d
Yash <starrai843@gmail.com>
2025-02-15 19:13:47 +0530

    minor changes

--- src/components/UI/HeroSection.jsx
+++ src/components/UI/HeroSection.jsx
@@ -1,13 +1,16 @@
 import image from "./img.jpg"
 import { FaLongArrowAltRight } from "react-icons/fa"
 export function HeroSection(){
+function changeLocation(){
+    window.location.href="/country"
+}
 return (
     <main className="hero-section">
         <div className="container grid grid-two-cols">
             <div className="hero-content">
                 <h1>Know all about the world</h1>
                 <p>Discover about each nation its history culture sort and search anything you need in this countrypedia</p>
-                <button>Start exploring <FaLongArrowAltRight></FaLongArrowAltRight></button>
+                <button onClick={()=>changeLocation()}>Start exploring <FaLongArrowAltRight></FaLongArrowAltRight></button>
             </div>
             <div className="hero-image" >
                 <img src={image}  alt="" width="400" height="250" / >
